63e22d315b51b38241501ca4a60fc2e0ad5af087
supunkamburugamuva <supun06@gmail.com>
2016-03-30 13:08:42 -0400

    improving readme

--- src/main/java/salsa/mdsaschisq/RotateManxcatMDS.java
+++ src/main/java/salsa/mdsaschisq/RotateManxcatMDS.java
@@ -971,7 +971,7 @@ public class RotateManxcatMDS {
             SetRotationMatrix(SubRotation[LocalRotationIndex], LocalRotationIndex,
                     param[PointVectorDimension + LocalRotationIndex][0], false);
         }
-        SetTotalRotation(Rotation, Scale, CurrentCosmicScaling, SubRotation[2], SubRotation[1], SubRotation[0], true);
+        SetTotalRotation(Rotation, Scale, CurrentCosmicScaling, SubRotation[2], SubRotation[1], SubRotation[0]);
 
     } // End SetupFinalTransformation
 
